06cf6c3782b6c1d1789dc1e66f6ea10c72d5e594
Galyfray <galyfra@gmail.com>
2020-11-18 20:14:58 +0100

    update user agent

--- build/createManifest.py
+++ build/createManifest.py
@@ -33,7 +33,7 @@ fileHolder = {
 }
 
 headers = {
-    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+    'User-Agent': 'pack formater/downloader'
 }
 
 #==#===#==# Functions #==#===#==#
